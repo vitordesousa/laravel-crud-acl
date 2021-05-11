5e0c1a69cdecd0094cc9ff05387a763bdcd92528
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-05-11 14:57:26 -0300

    showing post's author

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,8 @@
 		@forelse ($items as $item)
 			<h1>{{$item->title}}</h1>
 			<p>{{$item->description}}</p>
+			<p><strong>Autor: </strong> {{$item->user->name}}</p>
+			<hr>
 		@empty
 			<p class="alert alert-info">Nothing to show</p>
 		@endforelse		
