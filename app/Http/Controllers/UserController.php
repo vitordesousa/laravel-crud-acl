b2567a92828d936d6838c86a8584c06f6b6ffece
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-05-12 09:50:15 -0300

    created users.index with tables and pagination (bootstrap)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -13,7 +14,9 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+		//$posts = Post::all();
+		$users = User::paginate(10);
+		return view('users.index', compact('users'));
     }
 
     /**
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<table class="table table-bordered table-hover">
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Email</th>
+				<th>Created At</th>
+				<th>Actions</th>
+			</tr>
+		@forelse ($users as $user)
+			@can('view_post', $user)
+				<tr>
+					<td>{{$user->id}}</td>
+					<td>{{$user->name}}</td>
+					<td>{{$user->email}}</td>
+					<td>{{$user->created_at}}</td>
+					<td>
+						<a href="{{route('posts.show', $user->id)}}" class="btn btn-info btn-sm">Show</a> 
+						<a href="{{route('posts.edit', $user->id)}}" class="btn btn-warning btn-sm">Edit</a> 
+						<a href="{{route('posts.destroy', $user->id)}}" class="btn btn-danger btn-sm">Delete</a> 
+					</td>
+				</tr>
+			@endcan
+		@empty
+			<tr>
+				<td colspan="5"><strong>Nothing to show</strong></td>
+			</tr>
+		@endforelse		
+		</table>
+
+		{{$users->links()}}
+	</div>
+@endsection
