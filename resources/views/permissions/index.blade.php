653e1e13f7d557f7e1d8f41b59aab192f64a00ad
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-05-18 12:15:33 -0300

    fix permissions index

--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 	<div class="container">
+
+		@include('_components.alerts')
+
+		<div class="row">
+			<div class="col-lg-7 p-3">Permissions</div>
+			<div class="col-lg-5 p-3"><a href="{{route('permissions.create')}}" class="btn btn-success btn-sm float-right">Add</a> </div>
+		</div>
+		
 		<table class="table table-bordered table-hover">
 			<tr>
 				<th>ID</th>
@@ -11,32 +19,35 @@
 				<th>Created At</th>
 				<th>Actions</th>
 			</tr>
-		@forelse ($permissions as $permission)
-			@can('view_post', $permission)
+			@forelse ($permissions as $permission)
+				@can('view_post', $permission)
+					<tr>
+						<td>{{$permission->id}}</td>
+						<td>{{$permission->name}}</td>
+						<td>{{$permission->label}}</td>
+						<td>
+							@forelse ($permission->role as $role)
+								{{$role->name}}, 
+							@empty
+								-
+							@endforelse
+						</td>
+						<td>{{$permission->created_at}}</td>
+						<td>
+							<a href="{{route('permissions.edit', $permission->id)}}" class="btn btn-warning btn-sm">Edit</a> 
+							<a href="{{route('permissions.destroy', $permission->id)}}" class="btn btn-danger btn-sm" onclick="event.preventDefault(); document.getElementById('delete-form-{{$permission->id}}').submit();">Delete</a>
+							<form id="delete-form-{{$permission->id}}" action="{{ route('roles.destroy', $permission->id) }}" method="POST" class="d-none">
+								@csrf
+								@method('DELETE')
+							</form>
+						</td>
+					</tr>
+				@endcan
+			@empty
 				<tr>
-					<td>{{$permission->id}}</td>
-					<td>{{$permission->name}}</td>
-					<td>{{$permission->label}}</td>
-					<td>
-						@forelse ($permission->role as $role)
-							{{$role->name}}, 
-						@empty
-							
-						@endforelse
-					</td>
-					<td>{{$permission->created_at}}</td>
-					<td>
-						<a href="{{route('posts.show', $permission->id)}}" class="btn btn-info btn-sm">Show</a> 
-						<a href="{{route('posts.edit', $permission->id)}}" class="btn btn-warning btn-sm">Edit</a> 
-						<a href="{{route('posts.destroy', $permission->id)}}" class="btn btn-danger btn-sm">Delete</a> 
-					</td>
+					<td colspan="5"><strong>Nothing to show</strong></td>
 				</tr>
-			@endcan
-		@empty
-			<tr>
-				<td colspan="5"><strong>Nothing to show</strong></td>
-			</tr>
-		@endforelse		
+			@endforelse
 		</table>
 
 		{{$permissions->links()}}
