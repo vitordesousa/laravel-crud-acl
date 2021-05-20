61c48eec9ede86155213a1bb740baf75c33afc81
Vitor de Sousa <10253164+vitordesousa@users.noreply.github.com>
2021-05-20 01:24:04 -0300

    hidden content in all show pages

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="container">
-		<h1>{{$post->title}}</h1>
-		<p>{{$post->description}}</p>
-		<p><strong>Autor: </strong> {{$post->user->name}}</p>
-	</div>
+	@can('posts_show')
+		<div class="container">
+			<h1>{{$post->title}}</h1>
+			<p>{{$post->description}}</p>
+			<p><strong>Autor: </strong> {{$post->user->name}}</p>
+		</div>
+	@else
+		@include('_components.permission_denied')
+	@endcan
 @endsection
